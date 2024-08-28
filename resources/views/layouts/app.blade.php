9720ac80f0210a093bdab67f33d6dd38c74031c8
smkTexmacoRpl <fifahnadia@gmail.com>
2024-08-28 09:04:07 +0700

    commit Model merk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,9 @@
     <link rel="shortcut icon" href="./img/svg/logo.svg" type="image/x-icon" />
     <!-- Custom styles -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/fonts/stylesheet.css') }}">
-  
-  </head>
+    @yield('styles')
+       
+    </head>
 
   <body>
     <div class="layer"></div>
@@ -33,6 +33,7 @@
                 <div class="col-lg-12">
                 <div class="users-table table-wrapper">
                   {{-- isi --}}
+                  @yield('content')
                 </div>
               </div>
              
@@ -68,5 +69,10 @@
     <script src="{{ asset('assets/plugins/feather.min.js')}}"></script>
     <!-- Custom scripts -->
     <script src="{{ asset('assets/js/script.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    
+ @yield('scripts')
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.app');
 });
+Route::get('/tentangkami', function () {
+    return view('tentangkami');
+});
