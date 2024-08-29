186f4b8b390df9dade6c79cf8427841b4658dbc4
smkTexmacoRpl <fifahnadia@gmail.com>
2024-08-29 11:55:33 +0700

    commit edit fix

--- resources/views/merk/edit.blade.php
+++ resources/views/merk/edit.blade.php
@@ -3,13 +3,15 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 @section('content')
-    <div>
+    <div class="col-8 offset-2">
+        <h4>Edit Merk</h4>
+        <hr>
         <form action="{{ route('merk.update', $merk->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <input type="hidden" name="id" id="id" value="{{ $merk->id }}">
             <div class="mb-3">
-                <label for="">merk</label>
+                <label for="">merk barang</label>
                 <input type="text" class="form-control" id="" placeholder="Masukkan merk barang" name="merk"
                     value="{{ $merk->merk_barang }}">
             </div>
